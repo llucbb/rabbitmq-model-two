8d6c61b660ea30656ed0afa8665bf5be1fe7607c
llucbb <llucbb@gmail.com>
2020-04-14 17:14:06 +0200

    First commit

---
+++ src/main/java/org/llucbb/rabbitmqmodeltwo/entity/DummyMessage.java
@@ -0,0 +1,17 @@
+package org.llucbb.rabbitmqmodeltwo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DummyMessage {
+
+    private String content;
+
+    private int publishOrder;
+}
